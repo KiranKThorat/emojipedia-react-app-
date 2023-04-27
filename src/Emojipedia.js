6093c3050cb4fbfe846fbf54795c86f650b7ded2
kt <kthorat158@gmail.com>
2023-04-27 20:11:39 +0530

    added some more emojis

--- src/Emojipedia.js
+++ src/Emojipedia.js
@@ -34,12 +34,12 @@ const emojipedia = [
     meaning:
       "You are in love, sitting on cloud number nine. The romantic emoji with the enamored expression of the face stands for affairs of the heart."
   },
-      {
+  {
     id: 6,
     emoji: "😎",
     name: "Face With Sunglasses",
     meaning:
-    "I'm the king of the world! Mr. Cool Smiley with sunglasses. Is totally self-assured and relaxed. Everything is absolutely easy and cool. Also a symbol for sun, summer holidays, and holidays on the beach."
+      "I'm the king of the world! Mr. Cool Smiley with sunglasses. Is totally self-assured and relaxed. Everything is absolutely easy and cool. Also a symbol for sun, summer holidays, and holidays on the beach."
   },
   {
     id: 7,
@@ -47,9 +47,67 @@ const emojipedia = [
     name: "Pouting Smiley face",
     meaning:
       "The pouting face expresses displeasure with a person or situation. Beware, danger of explosion! Furious emoji face. Is annoyed and already sulking with a deep-red face."
+  },
+  {
+    id: 8,
+    emoji: "🤓",
+    name: "Nerd Face",
+    meaning: "Nerd emoji is a face with big glasses, an awkward smile and buck teeth. It's often used in a humorous or ironic way to describe nerds or to express how smart you are. "
+  },
+  {
+    id: 9,
+    emoji: "🤩",
+    name: "Smirking Face With Starry Eyes",
+    meaning: "Overwhelmed with excitement, the 🤩 emoji shows a face with a wide, open smile and stars for eyes, as if seeing a loved one. The color of the stars varies by platform, but is often gold or red"
+  },
+  {
+    id: 10,
+    emoji: "😏",
+    name: "Smirking/Whimsical Face",
+    meaning: "With a sideways glance, raised eyebrows and a half-smile, the 😏 emoji expresses smugness, irony, coolness or playfulness. It is often used to convey flirtation or sexual innuendo, but use it with caution because sometimes someone might be up to something"
+  },
+  {
+    id: 11,
+    emoji: "😞",
+    name: "Disappointed Face",
+    meaning: "This 😞 emoji shows a face with a sad expression, lowered gaze and down-turned corners of the mouth, reflecting disappointment and frustration."
+  },
+  {
+    id: 12,
+    emoji: "😟",
+    name: "Worried Face",
+    meaning: "the worried face 😟 reflects tension in the face of a complicated situation or important event. It shows open eyes, raised eyebrows and a wide frown"
+  },
+  {
+    id: 13,
+    emoji: "😕",
+    name: "Confused Face",
+    meaning: "A puzzled face 😕 expresses confusion or disagreement. The eyes are open and the forehead is tilted, as if the cheeks are pressed together or the lips are puckered."
+  },
+  {
+    id: 14,
+    emoji: "😣",
+    name: "Suffering Face",
+    meaning: "With narrowed eyes and a slightly furrowed brow, the 😣 emoji is an expression of helplessness and effort. It conveys the feeling of going through a difficult situation and suffering."
+  },
+  {
+    id: 15,
+    emoji: "😩",
+    name: " Weary Face",
+    meaning: "Leave me alone! With raised eyebrows and down-turned corners of the mouth, the 😩 emoji shows its dissatisfaction and exhaustion. Suffering from stressful, unpleasant but unavoidable events, it expresses a tearful, angry mood."
+  },
+  {
+    id: 16,
+    emoji: "🥺",
+    name: "Face with begging eyes",
+    meaning: "This 🥺 emoji shows a pleading, submissive expression with big, shiny eyes. It is used to ask for something, like money or to borrow a car."
   }
+
 
 ];
 
 export default emojipedia;
+
+
+
 
